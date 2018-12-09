518430c5df835258ed8a558b3e4f5d0526838595
Miwtoo <miwyugioh@gmail.com>
2018-12-09 16:30:52 +0700

    can save to local

---
+++ server/src/main/java/com/test/uploadpic/app/PicContoller.java
@@ -0,0 +1,52 @@
+package com.test.uploadpic.app;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+class PicContoller {
+    private static String UPLOADED_FOLDER = "D://User profile//Documents//GitHub//UploadPictureLocal-angular-spring//server//picture//";
+
+    @GetMapping
+    public String Hello() {
+        return "<h1>Hello</h1>";
+    }
+
+    @PostMapping("/upload")
+    public String UploadPic(@RequestParam("pic") MultipartFile file,RedirectAttributes redirectAttributes) {
+
+        if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+            return "redirect:uploadStatus";
+        }
+
+        try {
+
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+            Files.write(path, bytes);
+
+            redirectAttributes.addFlashAttribute("message",
+                    "You successfully uploaded '" + file.getOriginalFilename() + "'");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "fail";
+        }
+
+        return "redirect:/uploadStatus";
+    }
+
+}
